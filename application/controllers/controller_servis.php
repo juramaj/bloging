803bab39c51cd7baf7bfe0414f37e08fdea2df10
juramaj <juramaj1988@yandex.ru>
2015-01-07 08:47:34 +0200

    это что то

--- application/controllers/controller_servis.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Controller_Servis extends Controller
-{
-    function action_index()
-    {
-        $this->view->generate('servis_view.php', 'template_view.php');
-    }
-}
--- application/views/contacts_view.php
+++
@@ -1,4 +0,0 @@
-<h2><center>Контакты</center></h2>
-<p>SKYPE: ProkatCar</p>
-<p>E-MAIL: Prokatcer@gmail.com</p>
-<p>TEL: +3 8000 00 00 000</p>
